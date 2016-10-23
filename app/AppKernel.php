f5ae02f06cfbf111edf7d80663013c674af70b58
emna khemili <emna.khemili@gmail.com>
2016-10-23 22:47:54 +0100

    style sprint1

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,7 +15,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            //new AppBundle\AppBundle(),
             new front\GeneralBundle\frontGeneralBundle(),
             new back\GeneralBundle\backGeneralBundle(),
             new FOS\UserBundle\FOSUserBundle(),
