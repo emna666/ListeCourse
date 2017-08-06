efc3c5ea56b070545896f99a666a1ccedc886f85
emna khemili <emna.khemili@gmail.com>
2017-08-06 18:27:47 +0100

    recette

--- src/front/GeneralBundle/Controller/RecetteController.php
+++ src/front/GeneralBundle/Controller/RecetteController.php
@@ -2,12 +2,32 @@
 
 namespace front\GeneralBundle\Controller;
 
+use back\GeneralBundle\Entity\Recette;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class RecetteController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('', array('name' => $name));
+        $em = $this->get('doctrine.orm.entity_manager');
+
+        $recettes = $em->getRepository(Recette::class)->findAll();
+        return $this->render(':Front/recette/list:index.html.twig', array(
+            "recettes" => $recettes
+        ));
     }
+    public function detailsAction($id,Request $request)
+    {
+
+        $em = $this->get('doctrine.orm.entity_manager');
+        $recette = $em->getRepository(Recette::class)->find($id);
+        dump($recette);
+        if (!$recette)
+            throw  new \Exception("recette not found");
+        return $this->render(':Front/recette/details:index.html.twig', array(
+            "recette" => $recette
+        ));
+    }
+
 }
